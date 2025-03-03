ccaf57362e6ae35d9816b83a89a8ccfcb1cc336c
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-03-03 16:59:32 +0100

    Canvi a una branca nova

--- code/T4-PR1 App/Models/Simulacio.cs
+++ code/T4-PR1 App/Models/Simulacio.cs
@@ -6,33 +6,15 @@ namespace T4PR1
 {
     public class Simulacio
     {
-        public List<SistemaEnergia> simulacions = new List<SistemaEnergia>();
-
-        /// <summary>
-        /// Crea un nou sistema d'energia basat en l'opció seleccionada.
-        /// </summary>
-        /// <param name="opcio">L'opció seleccionada per l'usuari (1: Hidroelèctrica, 2: Eòlica, 3: Solar).</param>
-        /// <param name="dada">La dada específica del sistema seleccionat (cabal, velocitat del vent, hores de sol).</param>
-        /// <param name="rati">El rati de conversió de l'energia generada pel sistema.</param>
-        /// <param name="cost">El cost de creació del sistema.</param>
-        /// <param name="preu">El preu de venda de l'energia generada pel sistema.</param>
-        /// <returns>Un objecte de tipus SistemaEnergia corresponent a l'opció seleccionada.</returns>
-        //public void CrearSimulacio(int opcio, float dada, float rati, float cost, float preu)
-        //{
-        //    switch (opcio)
-        //    {
-        //        case 1:
-        //            simulacions.Add(new SistemaHidroelectric(DateTime.Now, dada, rati, cost, preu));
-        //            break;
-        //        case 2:
-        //            simulacions.Add(new SistemaEolic(DateTime.Now, dada, rati, cost, preu));
-        //            break;
-        //        case 3:
-        //            simulacions.Add(new SistemaSolar(DateTime.Now, dada, rati, cost, preu));
-        //            break;
-        //        default:
-        //            throw new ArgumentException(Missatges.OpcioEquivocadaMenuMissatge);
-        //    }
-        //}
+        public TipusSistema Tipus { get; set; }
+        public DateTime Data { get; set; }
+        public double Dada { get; set; }
+        public double Rati { get; set; }
+        public double Cost { get; set; }
+        public double Preu { get; set; }
+        public double EnergiaGenerada { get; set; }
+        public double CostTotal { get; set; }
+        public double PreuTotal { get; set; }
+        public double Benefici { get; set; }
     }
 }
--- code/T4-PR1 App/Pages/VeureSimulacions.cshtml.cs
+++ code/T4-PR1 App/Pages/VeureSimulacions.cshtml.cs
@@ -1,12 +1,37 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using T4PR1;
+using CsvHelper;
+using System.Globalization;
 
 namespace T4_PR1_App.Pages
 {
     public class VeureSimulacionsModel : PageModel
     {
+        public bool HasData { get; set; }
+        public List<Simulacio> Simulacions { get; set; }
         public void OnGet()
         {
+            Simulacions = new List<Simulacio>();
+            string filePath = "Files/Simulacions.csv";
+
+            try
+            {
+                if (System.IO.File.Exists(filePath))
+                {
+                    HasData = true;
+                    using var reader = new StreamReader(filePath);
+                    using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+                    foreach (var simulacio in csv.GetRecords<Simulacio>())
+                    {
+                        Simulacions.Add(simulacio);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                HasData = false;
+            }
         }
     }
 }
