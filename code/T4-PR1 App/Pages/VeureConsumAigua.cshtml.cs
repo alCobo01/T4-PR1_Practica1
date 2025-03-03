3db961e483c12819f352e6725b5884487bef0d44
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-03-03 20:10:08 +0100

    Pàgina per veure consum d'aigua feta, closes #8

--- code/T4-PR1 App/Pages/VeureConsumAigua.cshtml.cs
+++ code/T4-PR1 App/Pages/VeureConsumAigua.cshtml.cs
@@ -1,12 +1,41 @@
-using Microsoft.AspNetCore.Mvc;
+using CsvHelper;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+using T4_PR1_App.Models;
+using T4PR1;
 
 namespace T4_PR1_App.Pages
 {
     public class VeureConsumAiguaModel : PageModel
     {
+        public bool HasData { get; set; }
+        public List<ConsumAigua> Consums { get; set; }
         public void OnGet()
         {
+            Consums = new List<ConsumAigua>();
+            string filePath = "Files/consum_aigua_cat_per_comarques.csv";
+
+            try
+            {
+                if (System.IO.File.Exists(filePath))
+                {
+                    HasData = true;
+                    using var reader = new StreamReader(filePath);
+
+                    using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+                    var records = csv.GetRecords<ConsumAigua>().ToList();
+                    Consums.AddRange(records);
+                }
+                else
+                {
+                    HasData = false;
+                }
+            }
+            catch (Exception)
+            {
+                HasData = false;
+            }
         }
     }
 }
